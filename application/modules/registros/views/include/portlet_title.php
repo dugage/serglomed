23d77c202156350de1091da097a16b67fd49f3e5
kuiki <luigui916@gmail.com>
2019-04-10 14:47:03 +0200

    aquitando boton de crear registros a teleoperadoras

--- application/modules/registros/views/include/portlet_title.php
+++ application/modules/registros/views/include/portlet_title.php
@@ -1,6 +1,6 @@
 <div class="portlet-title">
 
-    <?php if($this->uri->segment(2)  != 'add'): ?>
+    <?php if($this->uri->segment(2)  != 'add' AND $rol != 4): ?>
 
         <a href="<?= site_url($path.'/add') ?>" class="btn green" type="button"><i class=" icon-plus "></i> Crear</a>
 
