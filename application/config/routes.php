e5a361731ae070b8a0ef3dba069dceae9ccbdc63
kuiki <luigui916@gmail.com>
2019-04-12 09:33:49 +0200

    agregando git ignore

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,6 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+//rutas para la home
+$route['panel'] = 'home';
 //rutas configuración
 $route['configuracion/roles'] = 'roles';
 $route['configuracion/roles/add'] = 'roles/add';
